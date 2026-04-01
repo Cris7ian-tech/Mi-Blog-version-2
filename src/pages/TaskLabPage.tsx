8b31f1c08d14dea16bf58f9dc61737a5003f2a71
unknown <Cristianlucero.ala2@gmail.com>
2026-04-01 12:22:39 -0300

    funcion createTask mas condicionales para gregar tareas

--- src/pages/TaskLabPage.tsx
+++ src/pages/TaskLabPage.tsx
@@ -1,45 +1,93 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-interface TaskLabProps {
-  id: string;
-  task: string;
-  name: string;
-  completed: boolean;
-}
+// interface TaskLabProps {
+//   id: string;
+//   task: string;
+//   name: string;
+//   completed: boolean;
+// }
+
 
 const TaskLabPage = () => {
+
   const [newTaskName, setNewTaskName] = useState(""); // [task, setTask]
-  //mostrar tareas en pantalla
-  const [taskItemens, setTaskItems] = useState([
+
+  //Mostrar Lista de tareas en pantalla
+  const [taskItems, setTaskItems] = useState([
     { id: 1, task: "Tarea 1", completed: false },
     { id: 2, task: "Tarea 2", completed: false },
     { id: 3, task: "Tarea 3", completed: false },
   ])
 
-  const handleCreateTask = (e: React.FormEvent<HTMLFormElement>) => { //sacamos error tipo any de e
+  //Creador de  nueva tarea comprobando que no exista en la lista para evitar duplicados, si no existe se agrega al estado de taskItems con un nuevo id generado por Date.now() y el estado de completed en false.
+  const createTask = () => {
+
+    alert("Creando Tarea: " + newTaskName);
+    //Ante todo Verificamos que no esté vacío (siempre es bueno)
+    if (newTaskName.trim() === "")  return;
+      
+    //Luego VERIFICACIÓN: ¿Ya existe?
+    const exists = taskItems.find(task => task.task.toLowerCase() === newTaskName.toLowerCase());
+    
+    if (!exists) {
+      //Si NO existe, lo agregamos a la lista de tareas
+      const updatedTasks = [...taskItems, { id: Date.now(), task: newTaskName, completed: false }];
+    setTaskItems(updatedTasks);
+
+    // Guardamos la LISTA COMPLETA en el local storage (como texto)
+    localStorage.setItem("task_list", JSON.stringify(updatedTasks))
+
+    setNewTaskName(""); // Limpiamos el input
+    } else {
+      // Si existe, avisamos
+    alert("¡Esa misión ya está asignada en el laboratorio!");
+    }
+
+    };
+
+    const handleCreateTask = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    localStorage.setItem("task", newTaskName);
-    setNewTaskName("");
-  }
+    createTask();
+  
+    }
+
+
 
   return (
+
     <>
+
     <section className="min-h-screen flex flex-col items-center justify-center p-8">
+
       {/* Card para Task Lab */}
+
       <div className="flex items-center justify-center max-w-7xl mx-auto px-6 py-20 text-center">
+
         <NavLink to="/tasklab">
+
           <h2 className="text-2xl font-bold text-white group-hover:text-[#DE8676]">
-            <span className="text-[#DE8676] group-hover:text-text-[#F2F2F2] text-5xl">Task</span> 
+
+            <span className="text-[#DE8676] group-hover:text-text-[#F2F2F2] text-5xl">Task</span>
+
             <span className="text-[#F2F2F2] group-hover:text-[#DE8676] text-5xl">Lab</span>
+
           </h2>
+
           <p className="text-[#A1A1A1] mt-2 text-xl">
+
             Gestor de misiones con almacenamiento local y lógica CRUD.
+
           </p>
+
         </NavLink>
+
       </div>
 
+
+
       <form onSubmit={handleCreateTask} >
+
         <input
           type="text"
           placeholder="Insert New task"
@@ -48,40 +96,70 @@ const TaskLabPage = () => {
           className="px-4 py-2 rounded-md bg-[#2D2F39] border border-[#373943] text-white focus:outline-none focus:ring-2 focus:ring-[#DE8676] w-full max-w-sm mx-auto mb-8"
         />
 
+
         <button
-          onClick={() => alert(newTaskName)}
+
+          type="submit"
           className="text-white border border-amber-500 p-1.5 ml-2.5"
         >
           Save Task
+
         </button>
 
+
+
         <button
+
           onClick={() => alert("borrado")}
           className="text-white border border-amber-200 p-1.5 ml-2.5"
         >
+
           Delete All
+
         </button>
+
       </form>
+
       {/* tabla de tareas */}
+
       <table className="mt-10 w-full max-w-sm border-separate border-spacing-y-2">
+        
         <thead>
+          
           <tr>
+
             <th className="text-[#DE8676] text-left text-xs uppercase tracking-[0.3em] pb-4">Misiones en curso</th>
+
           </tr>
+
         </thead>
+
         <tbody>
-          {taskItemens.map((item) => (
-            <tr key={item.id}>
-              <td className="text-white border border-[#A0A0A0] p-2 text-xl italic font-bold tracking-widest">{item.task}</td>
+
+          {taskItems.map((task) => (
+
+            <tr key={task.id}>
+
+              <td className="text-white border border-[#A0A0A0] p-2 text-xl italic font-bold tracking-widest">{task.task}</td>
+
             </tr>
+
           ))
+
           }
+
         </tbody>
+
       </table>
+
     </section>
-    
+
+
     </>
+
   );
 };    
+
+
 
 export default TaskLabPage
